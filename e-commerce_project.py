5a98af4ad9803709279ab5acb083dbc095cdcba4
Bhaskar <bhaskarjha95@gmail.com>
2025-01-29 11:06:31 +0000

    E-Commerce Website

---
+++ e-commerce_project.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+
+@app.route('/')
+def home():
+    products = Product.query.all()
+    return render_template('home.html', products=products)
+
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    product = Product.query.get_or_404(product_id)
+    return render_template('product_detail.html', product=product)
+
+@app.route('/add-to-cart/<int:product_id>')
+def add_to_cart(product_id):
+    if 'cart' not in session:
+        session['cart'] = []
+    session['cart'].append(product_id)
+    session.modified = True
+    return redirect(url_for('home'))
+
+@app.route('/cart')
+def cart():
+    cart_items = Product.query.filter(Product.id.in_(session.get('cart', []))).all()
+    return render_template('cart.html', cart_items=cart_items)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
